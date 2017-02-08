d0fe5c287038c7c0dff472ea2f8fcf4e497b5ec0
Rey Meljohn S. Guieb <reymeljohnguieb22@gmail.com>
2017-02-08 20:40:27 +0800

    Added files

---
+++ Exercise2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Exercise2{
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        InetAddress ia;
+        System.out.print("Enter host: ");
+        String input = kbd.nextLine();
+        for (int x = 1; x < 1024; x++) {
+            try {
+                Socket sock = new Socket(input, x);
+                System.out.println("Port: " + x +" ---Connection open!");
+            } catch (IOException ex) {
+            }
+        }
+    }  
+}
